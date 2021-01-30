62dfad8d3b11f28f4a0b239aba0d5f3db4515578
POISpider <35352424+usa2046@users.noreply.github.com>
2021-01-30 08:16:32 +0800

    Update dingTalk_msg.py

--- dingTalk_msg.py
+++ dingTalk_msg.py
@@ -22,7 +22,7 @@ def dingtalk(text):
     }
 
     headers = {'Content-Type': 'application/json;charset=utf-8'}
-    api_url = "https://oapi.dingtalk.com/robot/send?access_token=ba63349a096a9d8ba7c2a8bd9bc2dfa75285e78eeba2127c939754a73b84f541"
+    api_url = "https://oapi.dingtalk.com/robot/send?access_token="
     dingtalk_result = req.post(api_url, json.dumps(json_text), headers=headers).content    # 发送钉钉消息并返回发送结果
     with open('log.txt', mode='a+', encoding='gbk') as f:
         f.write("时间：" + str(time.localtime()) + "  发送状态：" + str(dingtalk_result) + "发送内容：" + str(text))
